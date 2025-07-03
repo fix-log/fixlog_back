2c279521cbd68613321b64ed45504e97698e358d
seonhongmun <mshg_@naver.com>
2025-07-03 23:54:57 +0900

    #fix nginx.conf 수정

--- scripts/run_daphne.sh
+++ scripts/run_daphne.sh
@@ -3,6 +3,11 @@
 
 set -e  # 에러 발생 시 스크립트 즉시 종료
 
+export DJANGO_SETTINGS_MODULE=config.settings.dev
+
+echo "▶️ 정적 파일 수집 시작..."
+poetry run python manage.py collectstatic --noinput
+
 echo "▶️ Daphne(WebSocket) 서버 시작..."
 exec poetry run daphne \
      -b 0.0.0.0 -p 8000 \

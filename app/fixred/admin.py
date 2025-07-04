005bb5c21a7cc17608d6440494da6d8fc9b003ed
nezeees <188424203+shinezeee@users.noreply.github.com>
2025-07-04 17:53:05 +0900

    #add: 픽레드 어드민 정의(초안)

--- app/fixred/admin.py
+++ app/fixred/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Fixred, FixredBlock, FixredComment, FixredImage, FixredReport
+
+
+class FixredImageInline(admin.TabularInline):
+    model = FixredImage
+    extra = 0
+
+
+class FixredCommentInline(admin.TabularInline):
+    model = FixredComment
+    extra = 0
+    readonly_fields = ("created_at",)
+
+
+# Fixred
+@admin.register(Fixred)
+class FixredAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "content_preview", "like_count", "comment_count", "read_permission", "created_at")
+    list_display_links = ("content_preview",)
+    search_fields = ("content", "user__nickname")
+    readonly_fields = ("created_at", "updated_at")
+    inlines = [FixredCommentInline, FixredImageInline]
+
+    def content_preview(self, obj):
+        return obj.content[:20]
+
+    content_preview.short_description = "내용 미리보기"
+
+
+# FixredImage
+@admin.register(FixredImage)
+class FixredImageAdmin(admin.ModelAdmin):
+    list_display = ("id", "post", "image")
+
+
+# FixredComment
+@admin.register(FixredComment)
+class FixredCommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "fixred", "user", "comment", "created_at")
+    search_fields = ("comment", "user__nickname")
+
+
+# FixredReport
+@admin.register(FixredReport)
+class FixredReportAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "fixred_post", "reason", "created_at")
+    list_filter = ("reason",)
+    search_fields = ("fixred_post__content", "user__nickname")
+
+
+# FixredBlock
+@admin.register(FixredBlock)
+class FixredBlockAdmin(admin.ModelAdmin):
+    list_display = ("id", "blocker", "blocked", "created_at")
+    search_fields = ("blocker__nickname", "blocked__nickname")

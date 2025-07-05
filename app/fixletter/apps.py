fd73bf2880bc1f191ccf0d0fbd1fd4fe4f2543b6
nezeees <188424203+shinezeee@users.noreply.github.com>
2025-07-05 17:14:09 +0900

    fix : release 파일 제외후 재커밋

--- app/fixletter/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FixletterConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "app.fixletter"
--- app/fixred/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class fixredConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "app.fixred"

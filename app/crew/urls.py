9463de5a56d187eb2d4b493835eae60307c9b08e
hak2881 <gkr054@naver.com>
2025-07-06 15:28:55 +0900

    수정

--- app/crew/urls.py
+++ app/crew/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/', views.ProjectListCreateAPIView.as_view(), name='project-list-create'),
-    path('projects/<int:pk>/', views.ProjectDetailAPIView.as_view(), name='project-detail'),
-    path('projects/<int:pk>/update/', views.ProjectUpdateAPIView.as_view(), name='project-update'),
-    path('projects/<int:pk>/delete/', views.ProjectDeleteAPIView.as_view(), name='project-delete'),
+    path("projects/", views.ProjectListCreateAPIView.as_view(), name="project-list-create"),
+    path("projects/<int:pk>/", views.ProjectDetailAPIView.as_view(), name="project-detail"),
+    path("projects/<int:pk>/update/", views.ProjectUpdateAPIView.as_view(), name="project-update"),
+    path("projects/<int:pk>/delete/", views.ProjectDeleteAPIView.as_view(), name="project-delete"),
 ]

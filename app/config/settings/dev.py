1bf51a4fb53239afc05f42fbf8c569989ba1ac33
ppomii <rubyhae@gmail.com>
2025-07-10 01:19:22 +0900

    #run_isort/black

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -1,11 +1,12 @@
-from .base import *
-
-from dotenv import load_dotenv
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+from .base import *
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-load_dotenv(BASE_DIR / ".env.local") # env 파일 설정
+load_dotenv(BASE_DIR / ".env.local")  # env 파일 설정
 
 DEBUG = True
 ENVIRONMENT = "local"
--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -1,11 +1,12 @@
-from .base import *
-
-from dotenv import load_dotenv
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+from .base import *
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-load_dotenv(BASE_DIR / ".env.prod") # env 환경 분리 설정
+load_dotenv(BASE_DIR / ".env.prod")  # env 환경 분리 설정
 
 DEBUG = False
 ENVIRONMENT = "production"

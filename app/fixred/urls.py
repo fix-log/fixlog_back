6a499a22ff0342fc446ab14d126f7a0ab448521a
nezeees <188424203+shinezeee@users.noreply.github.com>
2025-07-10 00:22:49 +0900

    #add : 게시글 상세조회 기능추가

--- app/fixred/urls.py
+++ app/fixred/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import FixredListView
 
 urlpatterns = [
-    path("list/", FixredListView.as_view(), name="fixred-list"),
+    path("", FixredListView.as_view(), name="fixred-list")
+    # path("/<itn:pk>/", FixredDetailView.as_view(), name="fixred-detail"),
 ]

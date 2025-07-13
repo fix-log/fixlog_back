f643a00fad88136be91f50ea39b8c79f7ea74c90
seonhongmun <mshg_@naver.com>
2025-07-14 01:41:56 +0900

    #fix 개발모드에서 staticfile 자동으로 가져오게 수정(local admin진입시 css 못가져오는거 수정)

--- app/config/urls/base.py
+++ app/config/urls/base.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -29,3 +32,6 @@ urlpatterns = [
     path("", include("app.accounts.urls")),
     path("fixred/", include("app.fixred.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

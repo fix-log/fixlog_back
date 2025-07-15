1896a88f9263a8569fd0a818c59e71b6e639f944
hak2881 <gkr054@naver.com>
2025-07-15 14:05:43 +0900

    Ref: 상대경로 절대경로로 수정

--- app/crew/admin.py
+++ app/crew/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, ProjectPosition, ProjectLanguage, ProjectSkillTool
+from app.crew.models import Project, ProjectPosition, ProjectLanguage, ProjectSkillTool
 
 
 class ProjectPositionInline(admin.TabularInline):

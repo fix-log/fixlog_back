e9d6c42c6c4fda65dcd7a65a5d550ad08b7c1a77
hak2881 <gkr054@naver.com>
2025-07-21 20:34:47 +0900

    run black, isort

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -61,6 +61,12 @@ CACHES = {
 # Application definition
 
 INSTALLED_APPS = [
+    "app.util",  # 유틸 기능
+    "app.accounts",  # 회원,인증
+    "app.fixred",  # fixred 관리
+    "app.crew",  # 크루(팀) 관리
+    "app.workroom",  # 워크룸 기능
+    "app.fixletter",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -72,13 +78,6 @@ INSTALLED_APPS += [
     "rest_framework",  # drf
     "rest_framework_simplejwt",  # JWT
     "drf_spectacular",  # 스웨거
-    # "app.fixed",  # Fixed 관리
-    "app.accounts",  # 회원,인증
-    "app.fixred",  # fixred 관리
-    "app.crew",  # 크루(팀) 관리
-    "app.workroom",  # 워크룸 기능
-    "app.fixletter",
-    "app.util",  # 유틸 기능
     "channels",
     "corsheaders",  # CORS 처리를 위한 앱
 ]

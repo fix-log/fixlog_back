9ffc606e57aa71c85e1a5b6db8b6cde6c9b77bc7
nezeees <188424203+shinezeee@users.noreply.github.com>
2025-07-22 19:23:59 +0900

    # add : 사용자 임시 어드민 추가

--- app/fixred/admin.py
+++ app/fixred/admin.py
@@ -55,3 +55,10 @@ class FixredReportAdmin(admin.ModelAdmin):
 class FixredBlockAdmin(admin.ModelAdmin):
     list_display = ("id", "blocker", "blocked", "created_at")
     search_fields = ("blocker__nickname", "blocked__nickname")
+
+from app.accounts.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "is_staff", "is_superuser")

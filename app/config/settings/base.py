ba5ed1e0a7781728b40120df818d58d40e3a9cd1
hak2881 <gkr054@naver.com>
2025-07-24 09:34:35 +0900

    fix: 오류 수정

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -144,7 +144,7 @@ else:
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
+        "NAME": os.getenv("POSTGRES_DB_TEST") if IS_TEST else os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
         "HOST": os.getenv("POSTGRES_HOST"),

4e1955683a5fb73291783192368a408704ce32c3
nezeees <188424203+shinezeee@users.noreply.github.com>
2025-07-28 02:10:08 +0900

    # add : 검색 히스토리 모델 추가

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
     "app.workroom",  # 워크룸 기능
     "app.fixletter", # 픽레터 기능 (현재 구현되지 않음)
     "app.notifications",  # 알림 기능
+    "app.search",  # 검색 기능 (검색 기록)
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

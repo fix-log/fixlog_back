899bb1f5ff261425b82abc6724e03ae5d614b89d
nezeees <188424203+shinezeee@users.noreply.github.com>
2025-08-01 02:00:16 +0900

    # add : 어드민

--- app/search/admin.py
+++ app/search/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SearchHistory
+
+
+@admin.register(SearchHistory)
+class SearchHistoryAdmin(admin.ModelAdmin):
+    list_display = ["user", "keyword"]
+    list_filter = ["user"]
+    search_fields = ["user__nickname", "keyword"]
+    ordering = ["-created_at"]

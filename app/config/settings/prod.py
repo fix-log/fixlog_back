e0d5bad8596d24c036aa3ed2a320ac871a50e415
S_H <mshg_@naver.com>
2025-08-06 00:46:45 +0900

    #fix 배포설정

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -10,3 +10,14 @@ load_dotenv(BASE_DIR / ".env.prod")  # env 환경 분리 설정
 
 DEBUG = False
 ENVIRONMENT = "production"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+    }
+}

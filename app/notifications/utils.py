d8e15315304703c7612fdf9b5cc55615e819d9f6
nezeees <188424203+shinezeee@users.noreply.github.com>
2025-08-12 21:26:05 +0900

    # add : 알림 온오프

--- app/notifications/utils.py
+++ app/notifications/utils.py
@@ -1,6 +1,9 @@
-from app.notifications.models import Notification
+from app.notifications.models import Notification, NotificationSetting
 
 
 # 알림 생성 함수
 def send_notification(user, sender, type_, event, target_id):
-    Notification.objects.create(user=user, sender=sender, notification_type=type_, event=event, target_id=target_id)
+    if not NotificationSetting.objects.filter(user=user, is_enabled=True).exists():
+        return Notification.objects.create(
+            user=user, sender=sender, notification_type=type_, event=event, target_id=target_id
+        )
